b135fc7c51882781038153c1d90b5511b7f16d02
AlexJ2005 <alexanderjernstrom@gmail.com>
2019-10-03 07:41:24 +0200

    initial commit

---
+++ client/build/precache-manifest.15e718b5ba7babcaf43ab9c2e02850b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b39acc07c2bf98400ec2b7e40a0b4c1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "162eafe8792299927f38",
+    "url": "/static/css/main.41b1bf37.chunk.css"
+  },
+  {
+    "revision": "d4f5d699fc34cdf981b3",
+    "url": "/static/js/2.fe9b27f9.chunk.js"
+  },
+  {
+    "revision": "162eafe8792299927f38",
+    "url": "/static/js/main.95120255.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "01ad5bbcfda7d221a54ed6d05c5ac531",
+    "url": "/static/media/shop.01ad5bbc.png"
+  }
+]);
