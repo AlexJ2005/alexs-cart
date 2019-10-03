bfe42566313e2be693a917e3a6314e1ee919a71c
AlexJ2005 <alexanderjernstrom@gmail.com>
2019-10-03 07:37:28 +0200

    initial commit

--- index.js
+++ index.js
@@ -7,32 +7,40 @@ const cors = require('cors');
 const mongoose = require('mongoose')
 const router = express.Router();
 const bodyParser = require('body-parser')
+const compression = require('compression');
 
 const itemsRoute = require('./routes/items');
 const editItem = require('./routes/editItem')
 const addItem = require('./routes/addItem');
+const path = require('path');
 
-app.use(cors());
+const PORT = process.env.PORT || 5000;
+const dev = app.get('env') !== 'production';
 
 mongoose.connect(process,env.MONGO_URI, { useNewUrlParser: true })
     .then(() => console.log('Database connected'))
 
 
-const PORT = process.env.PORT || 5000;
+
 
 app.use(bodyParser.json())
+app.use(cors());
 
 app.use('/api', itemsRoute);
+app.use('/addItem', addItem)
 
-if (process.env.NODE_ENV == 'production') {
-    app.use(express.static('client/build'));
+if(!dev){
+    app.disable('x-powered-by');
+    app.use(compression());
+    
+    app.use(express.static(path.resolve(__dirname, 'build')))
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
     })
 }
 
-app.use('/addItem', addItem)
+
 
 https.createServer({
     key: fs.readFileSync('server.key'),
