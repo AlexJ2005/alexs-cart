06012d0f41626f12ddaafc12df7280d87e364cb7
AlexJ2005 <alexanderjernstrom@gmail.com>
2019-10-07 20:14:49 +0200

    minor changes

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ if(!dev){
     app.use(express.static(path.resolve(__dirname, 'client/build')))
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+        res.sendFile(path.resolve(__dirname, '/client', '/build', 'index.html'))
     })
 }
 
@@ -42,3 +42,4 @@ https.createServer({
     console.log('App up and running')
   })
 
+console.log(path.resolve(__dirname, 'client', 'build' ,'index.html'))
