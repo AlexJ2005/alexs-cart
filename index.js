d0fa3863c731b0702f13510f3affc7470d74171c
AlexJ2005 <alexanderjernstrom@gmail.com>
2019-10-08 17:19:03 +0200

    minor changes to index.js

--- index.js
+++ index.js
@@ -5,12 +5,9 @@ const fs = require('fs');
 require('dotenv/config')
 const cors = require('cors');
 const mongoose = require('mongoose')
-const router = express.Router();
 const bodyParser = require('body-parser')
-const compression = require('compression');
 
 const itemsRoute = require('./routes/items');
-const editItem = require('./routes/editItem')
 const addItem = require('./routes/addItem');
 const path = require('path');
 
@@ -18,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 const dev = app.get('env') !== 'production';
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
-    .then(() => console.log('Database connected')).catch(err => console.log(err))
+.then(() => console.log('Database connected')).catch(err => console.log(err))
 
 app.use(bodyParser.json())
 app.use(cors());
@@ -26,12 +23,11 @@ app.use(cors());
 app.use('/api', itemsRoute);
 app.use('/addItem', addItem)
 
-if(!dev){
+if(process.env.NODE_ENV == 'production'){
     app.use(express.static('client/build'))
 
     app.get('*', (req, res) => {
-        const index = path.join(__dirname, 'client', 'build', 'index.html')
-        res.sendFile(path.resolve(index))
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     })
 }
 
